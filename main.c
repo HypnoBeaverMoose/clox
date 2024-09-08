54733f3acab1fe9189a702bb68718ffe5339c8a4
Yasen Atanasov <yasen@rara.co>
2024-09-08 13:03:56 +0300

    binary and unary operators

--- main.c
+++ main.c
@@ -11,10 +11,16 @@ int main()
     initChunk(&chunk);
 
     writeConstant(&chunk, 1.2, 123);
+    writeConstant(&chunk, 3.4, 123);
 
-    writeChunk(&chunk, OP_RETURN, 124);
+    writeChunk(&chunk, OP_ADD, 123);
 
-    //disassembleChunk(&chunk, "test");
+    writeConstant(&chunk, 5.6, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
 
     interpret(&chunk);
     freeChunk(&chunk);
--- vm.c
+++ vm.c
@@ -14,6 +14,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(OP)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a OP b);     \
+    } while (false)
 
     for (;;)
     {
@@ -38,6 +45,21 @@ static InterpretResult run()
             push(constant);
             break;
         }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+        case OP_NEGATE:
+            push(-pop());
+            break;
         case OP_RETURN:
             printValue(pop());
             return INTERPRET_OK;
@@ -45,6 +67,7 @@ static InterpretResult run()
     }
 #undef READ_CONSTANT
 #undef READ_BYTE
+#undef BINARY_OP
 }
 
 void initVM()
